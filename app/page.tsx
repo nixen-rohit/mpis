84106e90661eeaadc568ac0dd55f6600c48febd6
Rohit Chauhan <nixenboi@gmail.com>
2026-05-13 18:06:14 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
- 
-import Hero from "@/components/Hero"
-import CurvedLoop from '@/components/ui/CurvedLoop';
-
+import Hero from "@/components/Hero";
+import CurvedLoop from "@/components/ui/CurvedLoop";
+import { Testimonials } from "@/components/Testimonials";
+import Faq from "@/components/Faq"
 const page = () => {
   return (
     <div className="h-screen w-full ">
-  
-    <Hero />
-    {/* <CurvedLoop 
-  marqueeText="Be ✦ Creative ✦ With ✦ MPIS ✦"
-  speed={2}
-  curveAmount={200}
-  direction="right"
-  interactive
-  className="custom-text-style "
-/> */}
+      <Hero />
+
+      <CurvedLoop
+        marqueeText="Be ✦ Creative ✦ With ✦ MPIS ✦"
+        speed={2}
+        curveAmount={200}
+        direction="right"
+        interactive
+        className="custom-text-style "
+      />
+
+      <Testimonials /> 
+      <Faq/>
     </div>
-     
   );
 };
 
--- components/ui/CurvedLoop.tsx
+++ components/ui/CurvedLoop.tsx
@@ -120,7 +120,7 @@ const CurvedLoop: FC<CurvedLoopProps> = ({
       onPointerLeave={endDrag}
     >
       <svg
-        className="select-none w-full overflow-visible block aspect-[100/12] text-[6rem] font-bold uppercase leading-none"
+        className="select-none w-full overflow-visible block aspect-100/12 text-[6rem] font-bold uppercase leading-none"
         viewBox="0 0 1440 120"
       >
         <text ref={measureRef} xmlSpace="preserve" style={{ visibility: 'hidden', opacity: 0, pointerEvents: 'none' }}>
